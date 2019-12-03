336b6e1ded10b6b7ea9f341cf117608cea2ffe1e
kishorgpl <33367964+kishorgpl@users.noreply.github.com>
2019-12-03 13:24:22 +0530

    Add files via upload

---
+++ SpringHibernateExample/src/main/java/com/techm/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.techm.springmvc.dao;
+
+import java.util.List;
+
+import com.techm.springmvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
